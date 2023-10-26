0e020e7f31031ed8f1e22b8f0f878bc7ecf466c0
David Centeno <centeno70204@gmail.com>
2023-10-25 22:18:09 -0500

    entrega

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './NavBar';
+import ItemListContainer from './ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenido!" />
+    </div>
+  );
+  
+};
+
+export default App;
